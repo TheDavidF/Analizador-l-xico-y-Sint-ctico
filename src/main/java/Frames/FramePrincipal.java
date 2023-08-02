984b1003e20cf5d40b8cebfc9f9a5443d916e379
David Cano <davidcano202132346@cunoc.edu.gt>
2023-08-02 12:45:43 -0900

    Iniciando programación

--- src/main/java/Frames/FramePrincipal.java
+++ src/main/java/Frames/FramePrincipal.java
@@ -4,10 +4,8 @@
  */
 package Frames;
 
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
 
 /**
  *
@@ -20,41 +18,9 @@ public class FramePrincipal extends javax.swing.JFrame {
     public FramePrincipal() {
         initComponents();
         
-        labelPosicion = new JLabel("Line: 1, Column: 1");
-           
-        
-        textArea1.setLineWrap(true);
-        textArea1.addCaretListener(new CaretListener() {
-            @Override
-            public void caretUpdate(CaretEvent e) {
-                updatePositionLabel(textArea1);
-                updatePositionLabel(textArea2);
-            }
-        });
     }
 
-     private void updatePositionLabel(JTextArea textArea) {
-        try {
-            // Get the caret position
-            int caretPosition = textArea.getCaretPosition();
 
-            // Get the text from the JTextArea
-            String text = textArea.getText();
-
-            // Calculate the line and column numbers
-            int line = textArea.getLineOfOffset(caretPosition);
-            int column = caretPosition - textArea.getLineStartOffset(line);
-
-            // Adjust to display 1-based line and column numbers
-            line += 1;
-            column += 1;
-
-            // Update the position label
-            labelPosicion.setText("Line: " + line + ", Column: " + column);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
      
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -68,8 +34,8 @@ public class FramePrincipal extends javax.swing.JFrame {
         jMenuBar1 = new javax.swing.JMenuBar();
         menuArchivo = new javax.swing.JMenu();
         menuGenerarG = new javax.swing.JMenu();
-        menuAyuda = new javax.swing.JMenu();
         menuAcercade = new javax.swing.JMenu();
+        menuAyuda = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -79,7 +45,6 @@ public class FramePrincipal extends javax.swing.JFrame {
 
         jLabel1.setFont(new java.awt.Font("Yu Gothic UI", 0, 14)); // NOI18N
         jLabel1.setText("Error");
-        jLabel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
         textArea2.setColumns(20);
         textArea2.setRows(5);
@@ -91,10 +56,11 @@ public class FramePrincipal extends javax.swing.JFrame {
         menuGenerarG.setText("Generar grafico");
         jMenuBar1.add(menuGenerarG);
 
-        menuAyuda.setText("Ayuda");
-        jMenuBar1.add(menuAyuda);
-
         menuAcercade.setText("Acerca de");
+
+        menuAyuda.setText("Ayuda");
+        menuAcercade.add(menuAyuda);
+
         jMenuBar1.add(menuAcercade);
 
         setJMenuBar(jMenuBar1);
@@ -105,16 +71,14 @@ public class FramePrincipal extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(22, 22, 22)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                            .addContainerGap()
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 716, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
-                        .addContainerGap()
-                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 716, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGap(61, 61, 61)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(49, 49, 49)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 679, Short.MAX_VALUE)
+                            .addComponent(jScrollPane1))))
                 .addContainerGap(20, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -136,35 +100,9 @@ public class FramePrincipal extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(FramePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(FramePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(FramePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(FramePrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new FramePrincipal().setVisible(true);
-            }
-        });
+        FramePrincipal frame = new FramePrincipal();
+        frame.setVisible(true);           
+    
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
